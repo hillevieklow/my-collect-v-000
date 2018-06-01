75a65f7a4eff3c7614167f4b258e39fb1ba66f06
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-01 14:55:14 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(array)
-  i = 0 
+  i = 0
   new_collection = []
   while i < collection.length
-    new_collection << yeild(collection[i]) 
+    new_collection << yeild(collection[i])
     i += 1
   end
   new_collection
